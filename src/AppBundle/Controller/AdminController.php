6fef8ad45f0ee80515d24ec6c3eb67b4e233fed2
vlad242 <vlad242@i.ua>
2019-09-12 15:20:33 +0300

    config controllers

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,20 +2,110 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Episode;
+use AppBundle\Entity\Season;
+use AppBundle\Entity\Serial;
+use AppBundle\Form\NewEpisodeType;
+use AppBundle\Form\NewSeasonType;
+use AppBundle\Form\NewSerialType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Service\FileUploader;
 
-class DefaultController extends Controller
+class AdminController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/admin", name="admin_room")
      */
-    public function indexAction(Request $request)
+    public function showListAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('@App/default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $em = $this->getDoctrine()->getManager();
+        $serials = $em->getRepository('AppBundle:Serial')->findAll();
+        return $this->render('@App/Admin/adminView.html.twig', [
+            'serials' => $serials,
         ]);
     }
+
+    /**
+     * @Route("/new_serial", name="new_serial")
+     * @param Request $request
+     * @param FileUploader $fileUploader
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newSerialAction(Request $request, FileUploader $fileUploader)
+    {
+        $serial = new Serial();
+        $form = $this->createForm(NewSerialType::class, $serial);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $serial->getImagePath();
+            $fileName = $fileUploader->upload($image);
+            $serial->setImagePath($fileName);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($serial);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_room'));
+        }
+        return $this->render('@App/Admin/adminNewSerial.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/new_season/{serial}", name="new_season")
+     * @param Request $request
+     * @param Serial $serial
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newSeasonBySerialAction(Request $request, Serial $serial)
+    {
+        $season = new Season();
+        $form = $this->createForm(NewSeasonType::class, $season);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $season->setSerial($serial);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($season);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_room'));
+        }
+        return $this->render('@App/Admin/adminNewSeason.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/new_episode/{season}", name="new_episode")
+     * @param Request $request
+     * @param FileUploader $fileUploader
+     * @param Season $season
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newEpisodeBySeasonAction(Request $request, FileUploader $fileUploader, Season $season)
+    {
+        $episode = new Episode();
+        $form = $this->createForm(NewEpisodeType::class, $episode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $episode->getImagePath();
+            $fileName = $fileUploader->upload($image);
+            $episode->setImagePath($fileName);
+            $episode->setSeason($season);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($episode);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_room'));
+        }
+        return $this->render('@App/Admin/adminNewEpisode.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Episode;
+use AppBundle\Entity\Serial;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +11,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="show_serials")
      */
-    public function indexAction(Request $request)
+    public function showSerialsAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('@App/default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $em = $this->getDoctrine()->getManager();
+        $serials = $em->getRepository('AppBundle:Serial')->findAll();
+        return $this->render('@App/default/showSerials.html.twig', [
+            'serials' => $serials,
         ]);
     }
+
+
+    /**
+     * @Route("/season/{serial}", name="show_seasons")
+     * @param Request $request
+     * @param Serial $serial
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showSeasonBySerialAction(Request $request, Serial $serial)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $seasons = $em->getRepository('AppBundle:Season')->findBy([
+            'serial' => $serial
+        ]);
+        return $this->render('@App/default/showSeasons.html.twig', [
+            'seasons' => $seasons,
+            'serial' => $serial->getName()
+        ]);
+    }
+
+    /**
+     * @Route("/episode/{episode}", name="show_episode_detail")
+     * @param Request $request
+     * @param Episode $episode
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showEpisodeByIdAction(Request $request, Episode $episode)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $episode = $em->getRepository('AppBundle:Episode')->findOneBy([
+            'id' => $episode,
+        ]);
+        return $this->render('@App/default/showEpisode.html.twig', [
+            'episode' => $episode,
+        ]);
+    }
+
 }
